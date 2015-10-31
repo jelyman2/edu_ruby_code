27a25b74e07e81c289e2c3b2c5d4414ab7b26eaf
jelyman2 <jelyman@gmail.com>
2015-10-31 16:31:18 -0700

    updated sorting

--- sorting.rb
+++ sorting.rb
@@ -13,3 +13,17 @@ fruits = ["orange", "apple", "banana", "pear", "grapes"]
 fruits.sort! do |first, second|
   second <=> first
 end
+
+# Custom sorting method
+
+def alphabetize(arr, rev=false)
+  if rev
+    arr.reverse!
+  else
+    arr.sort!
+  end
+end
+
+numbers = [1,5,7,8,3092,184,192,119,205,192,22,339,10,88,55,44,433]
+
+puts alphabetize(numbers, rev=true)
